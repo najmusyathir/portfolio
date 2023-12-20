d233cb65f27dfdaa4c3d08af3469dc9a403a57b6
Dandelion Coder <dandelion@coder.com>
2023-12-20 15:44:12 +0800

    modify: update all required content in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+import Home from "./ui/Home";
+import About from "./ui/About";
+import Projects from "./ui/Project";
+import Contact from "./ui/Contact";
+
+import './index.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root/>}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contacts" element={<Contact />} />
+
+      </Route>
+    )
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
-export default App
+const Root = () => {
+
+  return (
+  <>
+    <div className="navbar">
+      <Navbar />
+    </div>
+    
+    <div className="content">
+      <Outlet/>
+    </div>
+
+  </>);
+}
+
+export function Navbar(){
+  return (
+  <div id="navbar">
+      <div id="nav_child1">
+          <a id="logo" href="https://www.google.com" className="font-medium px-3 py-2 text-slate-700 rounded-lg">Najmu Syathir</a>
+      </div>
+
+      <div id="nav_child2">
+          <nav className="nav_hlink">
+              <a href="/">Home</a>
+              <a href="/about" >About</a>
+              <a href="/projects">Projects</a>
+              <a href="/contacts">Contact Me</a>
+          </nav>
+      </div>
+      <button id="resume">My Resume</button>
+
+
+  </div>);
+}
+
+export default App;
